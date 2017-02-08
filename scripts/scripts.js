c15c53eba06b821497f4b692ae577f9687dfedd5
Craig Sharkie <craig.sharkie@gmail.com>
2017-02-09 07:05:04 +1100

    Applying Portfolio images and foundation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
 // Open external links in new Windows
 $("[rel=external]").attr("target", "_external");
 
+// Open Portfolio images in a light box
+$("[rel=portfolio]").each(function(event){
+    event.preventDefault();
+});
+
+
 // Make the copyright date dynamic
 $("footer time").html(function() {
     var date = new Date();
